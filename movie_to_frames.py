a70a07bddcc035d2a819b5a90902038b47ab8eab
kacwl <kac.wlodarczyk@gmail.com>
2020-11-13 23:12:39 +0100

    added method that extract frames from movie

---
+++ movie_to_frames.py
@@ -0,0 +1,16 @@
+from decord import VideoReader, cpu
+import numpy as np
+
+
+def extract_frames(path, skip=25):
+    with open(path, 'rb') as f:
+        vr = VideoReader(f, ctx=cpu(0))
+    # (batch_size, height, width, channels)
+    frames = vr.get_batch([range(0, len(vr), skip)]).asnumpy()
+    return np.swapaxes(frames, 1, 3)
+
+
+# path = r"C:\Users\kacwl\Downloads\test.mp4"
+
+# frames = extract_frames(path)
+# print(frames.shape)
