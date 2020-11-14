1fba563d5fac88a9b5fc7b608f94443b520f7127
hobogalaxy <zalewski.ukas@gmail.com>
2020-11-14 11:09:35 +0100

    update predict.py

--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+import torch
 from pytorch_lightning import LightningModule
 from torch.utils.data import DataLoader
 
@@ -7,62 +8,56 @@ from utils.transform import preprocess
 from main import load_config
 import csv
 
-
 # field names
 fields = [
-    "Name", "Amusement park",	"Animals Bench", "Building", "Castle", "Cave", "Church", "City", "Cross",
+    "Name", "Amusement park", "Animals", "Bench", "Building", "Castle", "Cave", "Church", "City", "Cross",
     "Cultural institution", "Food", "Footpath", "Forest", "Furniture", "Grass", "Graveyard", "Lake", "Landscape",
     "Mine", "Monument", "Motor vehicle", "Mountains", "Museum", "Open-air museum", "Park", "Person", "Plants",
     "Reservoir", "River", "Road", "Rocks", "Snow", "Sport", "Sports facility", "Stairs", "Trees", "Watercraft",
     "Windows"
 ]
 
-
 config = load_config()
 pretrained_model = LitModel.load_from_checkpoint("skyhacks2020/3kw3tr2z/checkpoints/epoch=0.ckpt", config=config)
 pretrained_model.freeze()
 
-
 test_dataset = SkyTestDataset(root_dir="data/skyhacks_hackathon_dataset/live_test_images", transforms=preprocess)
-data = []
+
+# data rows of csv file
+answers = []
 for img, filename in test_dataset:
 
     if img is None:
+        print(filename)
+        zeros = [0 for x in fields]
+        answers.append([filename] + zeros)
         continue
 
-    # print(filename)
     img = img.reshape((1, 3, 224, 224))
-    out = pretrained_model(img)
-    data.append([filename, *(out.tolist())])
-
-    # except Exception:
-    #     # print(filename)
-    #     zeros = [0 for x in fields]
-    #     data.append([filename, *zeros])
-
-print(len(data))
-print(data)
+    logits = pretrained_model(img)
+    logits = logits.squeeze()
+    preds = torch.where(logits > 0.5, 1, 0).tolist()
+    answers.append([filename] + preds)
 
 
-# print(data)
-# print(len(data))
-# print(out.size())
-# print(len(out.shape))
+def sortFunc(e):
+    return e[0]
 
 
-# # data rows of csv file
-# rows = []
-#
-# # name of csv file
-# filename = "task1_answers.csv"
-#
-# # writing to csv file
-# with open(filename, 'w') as csvfile:
-#     # creating a csv writer object
-#     csvwriter = csv.writer(csvfile)
-#
-#     # writing the fields
-#     csvwriter.writerow(fields)
-#
-#     # writing the data rows
-#     csvwriter.writerows(rows)
+answers = answers.sort(key=sortFunc)
+
+print(type(answers))
+
+# name of csv file
+filename = "task1_answers.csv"
+
+# writing to csv file
+with open(filename, 'w') as csvfile:
+    # creating a csv writer object
+    csvwriter = csv.writer(csvfile)
+
+    # writing the fields
+    csvwriter.writerow(fields)
+
+    # writing the data rows
+    csvwriter.writerows(answers)
