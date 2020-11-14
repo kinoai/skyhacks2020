4c5fb1b9580c3a63b394b72f73be44dea803cca0
Adrian Zieliński <adrianziel24@wp.pl>
2020-11-14 19:33:59 +0100

    Merge answers with yolo model

--- generate_answers.py
+++ generate_answers.py
@@ -1,10 +1,12 @@
 import torch
-from utils.lightning_wrapper import LitModel
-from utils.datasets import SkyTestDataset
-from utils.transform import test_preprocess
+from sky_utils.lightning_wrapper import LitModel
+from sky_utils.datasets import SkyTestDataset
+from sky_utils.transform import test_preprocess
+from sky_utils.models import YoloModel
 from main import load_config
 import csv
 
+from tqdm import tqdm
 
 # field names
 fields = [
@@ -19,11 +21,13 @@ config = load_config()
 pretrained_model = LitModel.load_from_checkpoint("skyhacks2020/1brh2881/checkpoints/epoch=8.ckpt", config=config)
 pretrained_model.freeze()
 
-test_dataset = SkyTestDataset(root_dir="skyhacks_hackathon_dataset/live_test_images", transforms=test_preprocess)
+yolo_model = YoloModel()
+
+test_dataset = SkyTestDataset(root_dir="skyhacks_hackathon_dataset/live_test_images", transforms=None)
 
 # data rows of csv file
 answers = []
-for img, filename in test_dataset:
+for img, filename in tqdm(test_dataset):
 
     if img is None:
         print(filename)
@@ -31,11 +35,22 @@ for img, filename in test_dataset:
         answers.append([filename] + zeros)
         continue
 
-    img = img.reshape((1, 3, 224, 224))
-    logits = pretrained_model(img)
+    try:
+        transformed_image = test_preprocess(img)
+    except Exception:
+        print(filename)
+        zeros = [0 for x in range(len(fields) - 1)]
+        answers.append([filename] + zeros)
+        continue
+
+    yolo_logits = yolo_model(img)
+    transformed_image = transformed_image.reshape((1, 3, 224, 224))
+    logits = pretrained_model(transformed_image)
     logits = logits.squeeze()
     preds = torch.where(logits > 0.5, 1, 0).tolist()
-    answers.append([filename] + preds)
+    yolo_preds = torch.where(yolo_logits > 0.5, 1, 0).tolist()
+    final_preds = [int(yolo_preds[i] or preds[i]) for i in range(len(preds))]
+    answers.append([filename] + final_preds)
 
 
 def sortFunc(e):
@@ -58,3 +73,5 @@ with open(filename, 'w', newline='') as csvfile:
 
     # writing the data rows
     csvwriter.writerows(answers)
+
+print(f'Answers saved to file {filename}')
